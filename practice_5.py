58793b8850f459072cb0d5e51c7902b1c0037caa
Timothy Davis <bori9876@gmail.com.com>
2026-02-03 17:23:31 -0500

    error counter and practice files

---
+++ practice_5.py
@@ -0,0 +1,14 @@
+def count_keywords_in_file(filename, keyword_list):
+    counts = {}
+    for key in keyword_list:
+        counts[key.lower()] = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            line_lower = line.lower()
+            for key in counts:
+                if key in line_lower:
+                    counts[key] += 1
+    return counts
+
+result = count_keywords_in_file('sample.log', ['ERROR', 'WARNING'])
+print(result)
---
+++ practice_6.py
@@ -0,0 +1,20 @@
+import sys
+
+default_message = "Enter at least 3 arguments"
+
+if len(sys.argv) < 3:
+    print(default_message)
+    sys.exit()
+
+def count_keywords_in_file(filename, keywords_list):
+    counts = {key.lower(): 0 for key in keywords_list}
+    with open(filename, 'r') as f:
+        for line in f:
+            line_lower = line.lower()
+            for key in counts:
+                if key in line_lower:
+                    counts[key] += 1
+    return counts
+
+result = count_keywords_in_file(sys.argv[1], sys.argv[2:])
+print(result\n)

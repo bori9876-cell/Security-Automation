42a27f9c21de407fb8bc2c8bd3b932cb2b519aa5
Timothy Davis <bori9876@gmail.com>
2026-06-10 08:18:57 -0400

    quick save

--- recipes3.py
+++ recipes3.py
@@ -25,11 +25,16 @@ def load_recipes():
 
 def display_recipe(recipe_name):
     print(f"Ingredients for {recipe_name}:")
-    step_number = 1
+
     for ingredients in recipes[recipe_name]["ingredients"]:
         print("-", ingredients.title())
+
     print()
+
     print(f"Instructions for {recipe_name}:")
+
+    step_number = 1
+    
     for instructions in recipes[recipe_name]["instructions"]:
         print(f"Step {step_number}: {instructions.capitalize()}")
         step_number += 1
@@ -40,23 +45,19 @@ def save_recipes():
 
 def search_recipes():
     craving = input("Enter the food you are craving: ")
+
     matches = []
+
     for recipe_name, recipe_data in recipes.items():
         if craving.lower() in recipe_data["ingredients"]:
             print("-", recipe_name.title())            
             matches.append(recipe_name)
+
     if matches:
         recipe_choice = input("Please select a recipe from the list: ").lower()
         if recipe_choice in matches:
-            step_number = 1
-            print(f"Ingredients for {recipe_choice}:")
-            for ingredients_list in recipes[recipe_choice]["ingredients"]:
-                print("-", ingredients_list.title())
-            print()
-            print(f"Instructions for {recipe_choice}:")
-            for instruction_steps in recipes[recipe_choice]["instructions"]:
-                print(f"Step {step_number}.", instruction_steps.capitalize())
-                step_number += 1
+            display_recipe(recipe_choice)
+
     if not matches:
         print("Sorry, there are no recipes with that ingredient available.")
 
@@ -64,35 +65,42 @@ def search_recipes():
 
 def add_recipe():
     recipe_name = input("Enter the name of the recipe you would like to add: ").lower()
+
     raw_ingredients = input("Enter the ingredients: ").split(",")
+
     instructions = input("Enter cooking instructions: ").split(",")
+
     clean_ingredients = []
+
     clean_instructions = []
+
     for item in raw_ingredients:
         clean_ingredients.append(item.strip())
+
     for item in instructions:
         clean_instructions.append(item.strip())
+
     recipes[recipe_name] = {
         "ingredients": clean_ingredients, "instructions": clean_instructions
     }
+
     save_recipes()
+
     print(recipe_name.title(), "added.")
 
 def view_all_recipes():
     print("All Recipes:")
+
     for recipe_name in recipes:
         print("-", recipe_name.title())
+
     print()
+
     select_recipe = input("Select a recipe: ").lower()
+    
     if select_recipe in recipes:
-        print(f"Ingredients for {select_recipe}:")
-        for ingredients_list in recipes[select_recipe]["ingredients"]:
-            print("-", ingredients_list.title())
-        print(f"Instructions for {select_recipe}:")
-        step_number = 1
-        for instruction_steps in recipes[select_recipe]["instructions"]:
-            print(f"Step {step_number}.", instruction_steps.capitalize())
-            step_number += 1
+        display_recipe(select_recipe)
+
     else:
         print("Recipe not found.")
 
@@ -100,21 +108,19 @@ def edit_recipe():
     if not recipes:
         print("No recipes to edit")
         return
+    
     recipe_option = 1
+
     for recipe_name in recipes:
         print(f"{recipe_option}. {recipe_name.title()}")
         recipe_option += 1
+
     print()
+
     choose_recipe = input("Which recipe would you like to edit?: ").lower().strip()
+
     if choose_recipe in recipes:
-        print(f"Ingredients for {choose_recipe}:")
-        for ingredients_list in recipes[choose_recipe]["ingredients"]:
-            print("-", ingredients_list.title())
-        print(f"Instructions for {choose_recipe}:")
-        step_number = 1
-        for instruction_steps in recipes[choose_recipe]["instructions"]:
-            print(f"Step {step_number}.", instruction_steps.capitalize())
-            step_number += 1
+        display_recipe(choose_recipe)
         while True:            
             edit_what = input("1: Edit ingredients?\n2: Edit instructions?\n3: Return to main menu\n Choose: ")
             if edit_what == "1":
@@ -144,10 +150,14 @@ def delete_recipe():
     if not recipes:
         print("No recipes to delete")
         return
+    
     for recipe_name in recipes:
         print("-", recipe_name.title())
+
     print()
+
     to_remove = input("Choose which recipe to delete :").lower().strip()
+
     if to_remove in recipes:
         while True:
             confirm = input(f"Are you sure you want to delete {to_remove}? 1. Yes\n 2. No").lower().strip()
@@ -161,6 +171,7 @@ def delete_recipe():
                 break
             else:
                 print("Invalid choice, try again.")
+
     else:
         print("Recipe not found.")
 
@@ -170,14 +181,19 @@ while True:
     menu_choice = input("Please choose from the following options:\n 1. Search Recipes\n 2. Add Recipe\n 3. View All Recipes\n 4. Delete Recipe\n 5. Edit Recipe\n 6. Exit\n")
     if menu_choice == "1":
         search_recipes()
+
     elif menu_choice == "2":
         add_recipe()
+
     elif menu_choice == "3":
         view_all_recipes()
+
     elif menu_choice == "4":
         delete_recipe()
+
     elif menu_choice == "5":
         edit_recipe()
+
     elif menu_choice == "6":
         print("Goodbye")
         break
